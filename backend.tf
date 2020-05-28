edfd3a175e0919d181700c54ce0777ea25c76cf1
nalbam <me@nalbam.com>
2020-05-28 15:11:52 +0900

    move to cloud

--- backend.tf
+++ backend.tf
@@ -1,25 +1,25 @@
 # backend
 
-terraform {
-  backend "s3" {
-    region = "ap-northeast-2"
-    bucket = "terraform-nalbam-seoul"
-    key    = "opspresso.tfstate"
-  }
-  required_version = ">= 0.12"
-}
-
 # terraform {
-#   backend "remote" {
-#     organization = "nalbam"
-#     workspaces {
-#       name = "web-nalbam"
-#     }
+#   backend "s3" {
+#     region = "ap-northeast-2"
+#     bucket = "terraform-nalbam-seoul"
+#     key    = "opspresso.tfstate"
 #   }
+#   required_version = ">= 0.12"
 # }
+
+terraform {
+  backend "remote" {
+    organization = "opspresso"
+    workspaces {
+      name = "web-opspresso"
+    }
+  }
+}
 
 provider "aws" {
   region = var.region
 }
 
-# aws s3 cp s3://terraform-nalbam-seoul/www.tfstate ./terraform.tfstate
+# aws s3 cp s3://terraform-nalbam-seoul/opspresso.tfstate ./terraform.tfstate
