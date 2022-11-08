f3fe825b28dc4550592359ed095097fb7cfb44bb
nalbam <me@nalbam.com>
2022-11-08 15:05:21 +0900

    add

--- main.tf
+++ main.tf
@@ -1,38 +1,24 @@
 # main
 
-# module "domain" {
-#   source = "github.com/nalbam/terraform-aws-route53?ref=v0.12.31"
+module "domain" {
+  source = "github.com/nalbam/terraform-aws-route53?ref=v0.12.31"
 
-#   root_domain = var.domain
+  root_domain = var.domain
 
-#   # acm_certificate = true
-# }
+  # acm_certificate = true
+}
 
-# module "www" {
-#   source = "github.com/nalbam/terraform-aws-static-web?ref=v0.12.22"
-#   region = var.region
+module "www" {
+  source = "github.com/nalbam/terraform-aws-static-web?ref=v0.12.22"
+  region = var.region
 
-#   zone_id         = module.domain.zone_id
-#   certificate_arn = module.domain.certificate_arn
+  zone_id         = module.domain.zone_id
+  certificate_arn = module.domain.certificate_arn
 
-#   force_destroy = true
+  force_destroy = true
 
-#   domain_name = [
-#     format("www.%s", var.domain),
-#     var.domain,
-#   ]
-# }
-
-# module "repo" {
-#   source = "github.com/nalbam/terraform-aws-static-web?ref=v0.12.22"
-#   region = var.region
-
-#   zone_id         = module.domain.zone_id
-#   certificate_arn = module.domain.certificate_arn
-
-#   force_destroy = true
-
-#   domain_name = [
-#     format("repo.%s", var.domain),
-#   ]
-# }
+  domain_name = [
+    format("www.%s", var.domain),
+    var.domain,
+  ]
+}
