31d32675f00dc92302cfe81f00dbc49aa4ff43a0
nalbam <me@nalbam.com>
2022-11-09 16:24:15 +0900

    rm www

--- main.tf
+++ main.tf
@@ -5,20 +5,20 @@ module "domain" {
 
   root_domain = var.domain
 
-  # acm_certificate = true
+  acm_certificate = true
 }
 
-module "www" {
-  source = "github.com/nalbam/terraform-aws-static-web?ref=v0.12.22"
-  region = var.region
+# module "www" {
+#   source = "github.com/nalbam/terraform-aws-static-web?ref=v0.12.22"
+#   region = var.region
 
-  zone_id         = module.domain.zone_id
-  certificate_arn = module.domain.certificate_arn
+#   zone_id         = module.domain.zone_id
+#   certificate_arn = module.domain.certificate_arn
 
-  force_destroy = true
+#   force_destroy = true
 
-  domain_name = [
-    format("www.%s", var.domain),
-    var.domain,
-  ]
-}
+#   domain_name = [
+#     format("www.%s", var.domain),
+#     var.domain,
+#   ]
+# }
